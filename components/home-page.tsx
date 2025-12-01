d3eb2d37bffd2d22d23b68b7e642472fa941be48
Piyush <87140377+TheDevPiyush@users.noreply.github.com>
2025-12-01 12:34:40 +0530

    Update email link to use dynamic personal email

--- components/home-page.tsx
+++ components/home-page.tsx
@@ -109,7 +109,7 @@ export function HomePage({ data }: HomePageProps) {
                   style={{ background: 'var(--gradient-secondary)', color: 'rgb(var(--color-text-primary))' }}
                   className="hover:opacity-90"
                 >
-                  <Link href="mailto:piyushdev.developer@gmail.com">
+                  <Link href={`mailto:${personal.email}`}>
                     <Mail className="w-5 h-5 mr-2" />
                     Get In Touch
                   </Link>
